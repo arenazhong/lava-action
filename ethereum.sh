6115bbf98cab385c853d23c5434d5adae6aeaf9a
arenazhong <154510501+arenazhong@users.noreply.github.com>
2024-03-27 09:50:23 +0000

    .

--- ethereum.sh
+++ ethereum.sh
@@ -2,7 +2,10 @@
 
 RPC_URLS=(
     "https://eth1.lava.build/lava-referer-d169b15b-5984-454f-9b1f-f908bae1443e/",
-    "https://eth1.lava.build/lava-referer-f0e1b816-37da-4a4c-86aa-d4082739bb3d/",
+    "https://eth1.lava.build/lava-referer-1802659e-6abe-41f5-9ea5-db37d5eb00af/",
+    "https://eth1.lava.build/lava-referer-97547cf9-6b48-48ec-aef8-a62bf9a841b4/",
+    "https://eth1.lava.build/lava-referer-1ae8cb7b-9a3d-4b3f-9a02-725e513f6c67/",
+    "https://eth1.lava.build/lava-referer-f0e1b816-37da-4a4c-86aa-d4082739bb3d/"
 )
 
 OUTPUT_DIR="logs"
